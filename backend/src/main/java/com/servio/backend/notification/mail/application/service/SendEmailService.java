4fcf5cd7858c81bd0f6f1c027dc225f04aab865d
Damianpiazz <damipiazza333@gmail.com>
2026-03-14 19:15:36 -0300

    feat(notificacion): agregar servicio de aplicacion SendEmailService

--- backend/src/main/java/com/servio/backend/notification/mail/application/service/SendEmailService.java
+++ backend/src/main/java/com/servio/backend/notification/mail/application/service/SendEmailService.java
@@ -1,4 +1,23 @@
 package com.servio.backend.notification.mail.application.service;
 
-public class SendEmailService {
+import com.servio.backend.notification.mail.application.port.in.SendEmailUseCase;
+import com.servio.backend.notification.mail.application.port.out.EmailSenderPort;
+import com.servio.backend.notification.mail.domain.Email;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SendEmailService implements SendEmailUseCase {
+
+    private final EmailSenderPort emailSenderPort;
+
+    @Override
+    public void send(Email email) {
+        log.info("Enviando email a {} con asunto '{}'", email.getTo(), email.getSubject());
+        emailSenderPort.send(email);
+        log.info("Email enviado correctamente a {}", email.getTo());
+    }
 }
