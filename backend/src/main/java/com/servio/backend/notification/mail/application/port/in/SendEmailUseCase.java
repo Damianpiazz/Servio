23094a3616ce1c63d46b52fb73bb6046eea1c2d6
Damianpiazz <damipiazza333@gmail.com>
2026-04-09 22:35:28 -0300

    chore(audit): configurar propiedades de JaVers en dev, docker y test

--- backend/src/main/java/com/servio/backend/notification/mail/application/port/in/SendEmailUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.servio.backend.notification.mail.application.port.in;
-
-import com.servio.backend.notification.mail.domain.Email;
-
-public interface SendEmailUseCase {
-    void send(Email email);
-}
--- backend/src/main/java/com/servio/backend/notification/mail/application/port/out/EmailSenderPort.java
+++
@@ -1,7 +0,0 @@
-package com.servio.backend.notification.mail.application.port.out;
-
-import com.servio.backend.notification.mail.domain.Email;
-
-public interface EmailSenderPort {
-    void send(Email email);
-}
